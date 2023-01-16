4175471a0b99ecffa80a0b64744a05941b2056d7
hyemib <hyem0127@gmail.com>
2023-01-16 15:42:42 +0900

    Initial Setting

---
+++ SnapkitPractice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SnapkitPractice
+//
+//  Created by hyemi on 2023/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.title = "뷰턴트롤러 No 스토리보드 - swift"
+        self.view.backgroundColor = .systemMint
+    }
+
+
+}
+
